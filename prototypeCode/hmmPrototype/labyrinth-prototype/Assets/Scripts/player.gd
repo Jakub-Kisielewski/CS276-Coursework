0c11f18afb1ec29cce42608124065562983408d9
blackyistar <dayoibimidun1@gmail.com>
2025-11-01 11:48:22 +0000

    Seperated movement and attack logic

--- prototypeCode/hmmPrototype/labyrinth-prototype/Assets/Scripts/player.gd
+++ prototypeCode/hmmPrototype/labyrinth-prototype/Assets/Scripts/player.gd
@@ -1,10 +1,6 @@
 extends CharacterBody2D
 
-
-@onready var hitbox = $AnimatedSprite2D/hitBox
-
-const DASH_COOLDOWN_TIME = 0.8
-const DASH_DURATION_TIME = 0.3
+@onready var attack = $AnimatedSprite2D
 var health = 100.0
 var speed = 300.0
 var dead = false
@@ -14,34 +10,17 @@ enum Facing {UP, DOWN, LEFT, RIGHT}
 var player_facing : Facing
 var attacking = false
 var attacked = false
-
-var dash_multiplier = 3
-var dash_cooldown = 0.0
-var dash_timer = 0.0
-var is_dashing = false
+var dashing = false
 
 func _physics_process(delta: float) -> void:
-	
-	
-
-	
-	hitbox.monitoring = false
-	hitbox.get_node("strikeCollisionShape").disabled = true
 	handle_movement(delta)
 
 	if Input.is_action_pressed("attack_basic"):
-		attacking = true
-		handle_attack()
-		
-
-
-
+		attack.handle_attack()
+	elif Input.is_action_just_pressed("dash"):
+		attack.handle_dash();
 	
 func handle_movement(delta: float):
-	
-	if dash_cooldown > 0.0:
-		dash_cooldown -= delta
-	
 	if Input.is_action_pressed("move_up"):
 		direction.y = -1
 		player_facing = Facing.UP
@@ -63,69 +42,13 @@ func handle_movement(delta: float):
 		direction.x = 0
 
 	direction = direction.normalized()
-	
-	if Input.is_action_just_pressed("dash") and dash_cooldown <= 0.0:
-		if direction == Vector2.ZERO:
-			match player_facing:
-				Facing.UP: direction = Vector2.UP
-				Facing.DOWN: direction = Vector2.DOWN
-				Facing.LEFT: direction = Vector2.LEFT
-				Facing.RIGHT: direction = Vector2.RIGHT
 		
-		is_dashing = true
-		dash_timer = DASH_DURATION_TIME
-		dash_cooldown = DASH_COOLDOWN_TIME
-		$AnimatedSprite2D.play("jump")
-		$AnimatedSprite2D/hurtBox/hurtCollisionShape.disabled = true
-		
-		
-	if is_dashing:
-		velocity = direction * speed * dash_multiplier
+	if dashing:
+		velocity = direction * speed * attack.dash_multiplier
 		move_and_slide()
-		
-		dash_timer -= delta
-		
-		if dash_timer <= 0.0:
-			is_dashing = false
-			$AnimatedSprite2D/hurtBox/hurtCollisionShape.disabled = false
-			
-	
 	else:
 		velocity = direction * speed
-		move_and_slide()
-	
-func handle_attack():
-	$AnimatedSprite2D.flip_h = facing_left
-	$AnimatedSprite2D.play("attack_basic")
-	
-	if player_facing == Facing.UP:
-		hitbox.position = Vector2(0, -20)
-		hitbox.rotation = -PI/2
-	elif player_facing == Facing.DOWN:
-		hitbox.position = Vector2(0, 20)
-		hitbox.rotation = PI/2
-	elif player_facing == Facing.LEFT:
-		hitbox.position = Vector2(-20, 0)
-		hitbox.rotation = PI
-	elif player_facing == Facing.RIGHT:
-		hitbox.position = Vector2(20, 0)
-		hitbox.rotation = 0
-		
-	hitbox.monitoring = true
-	hitbox.get_node("strikeCollisionShape").disabled = false
-	
-	
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if $AnimatedSprite2D.animation == "attack_basic":
-		attacking = false
-		hitbox.monitoring = false
-		hitbox.get_node("strikeCollisionShape").disabled = true
-		
-	#if $AnimatedSprite2D.animation == "death":
-		#get_tree().quit()
-		
-
-		
+		move_and_slide()	
 
 func take_damage(damage : int) -> void:
 	health -= damage
@@ -137,17 +60,3 @@ func take_damage(damage : int) -> void:
 	attacked = true #use this if you want to play uninterrupted hit animation
 	print("player has been damaged")
 	print("health:", health)
-	
-	
-		
-
-
-
-func _on_animated_sprite_2d_frame_changed() -> void:
-	if $AnimatedSprite2D.animation == "attack":
-		var current_frame = $AnimatedSprite2D.frame
-		
-		if current_frame in [3,4,5]:
-			hitbox.get_node("strikeCollisionShape").disabled = false
-		else:
-			hitbox.get_node("strikeCollisionShape").disabled = true
