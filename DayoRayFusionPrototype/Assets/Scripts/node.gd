c8b5fd77ebdc9ac5102a252fb48aade38682d1cf
blackyistar <dayoibimidun1@gmail.com>
2025-11-04 17:25:39 +0000

    Fixed Problems
    Level progression works, boss can only be attacked when stunned, increased boss stun duration and player block duration, improved sword attack mechanics

---
+++ DayoRayFusionPrototype/Assets/Scripts/node.gd
@@ -0,0 +1,36 @@
+extends Node
+
+@onready var rewardControl = $CanvasLayer/Control
+
+var enemies_alive = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies_alive = enemies.size()
+	print("Enemies remaining: ", enemies_alive)
+	
+	for enemy in enemies:
+		if enemy.has_signal("died"):
+			enemy.connect ("died", Callable(self, "_on_enemy_died"))
+		else:
+			enemy.connect("tree_exited", Callable(self, "_on_enemy_died"))
+			
+func _on_enemy_died():
+	enemies_alive -= 1
+	print("enemies remaining: ", enemies_alive)
+	if enemies_alive <= 0:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		print("Choose your reward")
+		rewardControl.visible = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+		
+	if Input.is_key_pressed(KEY_ENTER):
+		if get_tree().current_scene.scene_file_path == "res://Assets/Scenes/area_1.tscn":
+			get_tree().change_scene_to_file("res://Assets/Scenes/area_2.tscn")
+		else:
+			get_tree().change_scene_to_file("res://Assets/Scenes/area_3.tscn")
